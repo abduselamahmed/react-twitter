d9c5552ec7866e40e63192f34a818f2c87cd55ba
Bytewar <bytewar@github.com>
2019-02-15 20:44:45 +0300

    Did the header section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react'
+import { Header } from './components/Header'
+import "./components/Header.css"
 
-class App extends React.Component {
+class App extends Component {
   render() {
     return (
       <div>
-        <h1>noTwitter</h1>
+        <Header text="Bytewar" />
+        <h1>notTwitter</h1>
+        <p>by <a href="/bytewar">@bytewar</a></p>
       </div>
     );
   }
